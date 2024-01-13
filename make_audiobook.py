46e94e9eb42c9a9f57d15b8c1d8bbc9f180eff75
YongeBai <yongebai@gmail.com>
2024-01-13 15:01:31 +0800

    pushint to test with gpu

--- make_audiobook.py
+++ make_audiobook.py
@@ -1,15 +1,18 @@
 import os
 import torch
+from TTS.api import TTS
+from argparse import ArgumentParser
 
-from utils import process_metadata
-
-
-BOOK_TITLE = "Example"
-AUTHOR = "Example"
-PATH_TO_TXT_FILES = os.path.join(os.getcwd(), "books", BOOK_TITLE, "chapters_text")
+# from utils import process_metadata
 
 
-def main():
+cuda = torch.cuda.is_available()
+device = torch.device("cuda" if cuda else "cpu")
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=cuda)
+# tts.to(device)
+
+
+def main(voice_prompt_file: str):
     chapter_num = 1
     files = os.listdir(PATH_TO_TXT_FILES)
     files.sort(
@@ -18,19 +21,43 @@ def main():
         )
     )
 
-    for file in files:
-        full_path = os.path.join(PATH_TO_TXT_FILES, file)
+    for file in files[:1]:
+        text_path = os.path.join(PATH_TO_TXT_FILES, file)
+        audio_path = os.path.join(PATH_TO_AUDIO_FILES, file)
 
         # read chapter
+        with open(text_path, "r", encoding="utf-8") as f:
+            text = " ".join([l for l in f.readlines()])
+            tts.tts_to_file(
+                text=text,
+                file_path=audio_path,
+                speaker_wav=voice_prompt_file,
+                language="en",
+                split_sentences=True,
+            )
 
         # rvc
 
         # process metadata
-        process_metadata(full_path, BOOK_TITLE, chapter_num, AUTHOR)
-        chapter_num += 1
+        # process_metadata(full_path, BOOK_TITLE, chapter_num, AUTHOR)
+        # chapter_num += 1
 
 
 if __name__ == "__main__":
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    parser = ArgumentParser()
+    parser.add_argument("--title", type=str, required=True, default=None)
+    # parser.add_argument("--author", type=str, default=None)
+    parser.add_argument("--voice", type=str, default="lex_fridman")
+    
+    args = parser.parse_args()
 
-    main()
+    BOOK_TITLE = args.title
+    # AUTHOR = args.author
+    VOICE = args.narrator
+
+    PATH_TO_TXT_FILES = os.path.join(os.getcwd(), "books", BOOK_TITLE, "chapters_text")
+    PATH_TO_AUDIO_FILES = os.path.join(os.getcwd(), "books", BOOK_TITLE, "chapters_audio")
+    PATH_TO_VOICES = os.path.join(os.getcwd(), "voices")
+    voice_prompt_file = os.path.join(PATH_TO_VOICES, f"{VOICE}.wav")
+
+    main(voice_prompt_file)
--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ def process_metadata(file_path: str, book_title: str, chapter_no: int, author: s
 
     audio["album"] = book_title
     audio["tracknumber"] = chapter_no
-    audio["artist"] = author
+    # audio["artist"] = author
     audio["title"] = file_path[:-4]
 
     audio.save()
